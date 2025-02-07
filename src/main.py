a8151572d73f8d023303324dc83fbdd280982efb
JoysET-source <joys7919@gmail.com>
2025-02-07 23:02:18 +0200

    web developer under work

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ricette.db'  # Sostituisci co
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-ricette_path = os.path.join("/static/db_Ricette")
+ricette_path = os.path.join("static", "Ricette")
 
 def load_ricette(categoria):
     categoria_path = os.path.join(ricette_path, categoria)
@@ -16,15 +16,17 @@ def load_ricette(categoria):
     if os.path.exists(categoria_path):
         for filename in os.listdir(categoria_path):
             if filename.endswith(".jpg"):
-                image_path = os.path.join("db_Ricette", categoria, filename)
-                recipe_txt = filename.replace(".jpg",".txt")
+                image_path = os.path.join("Ricette", categoria, filename)  # Rimuovi "static" dal percorso
+                recipe_txt = filename.replace(".jpg", ".txt")
                 txt_path = os.path.join(categoria_path, recipe_txt)
                 if os.path.exists(txt_path):
                     with open(txt_path, "r") as f:
                         description = f.read()
                         ricette.append({"image": image_path, "description": description})
+                        # print(f"Caricata ricetta: {filename}, descrizione: {description}") questi print aiutano il debug se non passano i dati richiesti
+    # else:
+        # print(f"Categoria '{categoria}' non trovata in {categoria_path}") come altro print
     return ricette
-
 
 @app.route('/')
 def home():
